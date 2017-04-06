0d15cffb1c6b00f1cb2a0a1fc1ffb9174a89e2a6
Sara <sarah.alalaya@gmail.com>
2017-04-07 00:37:53 +0300

    Player one can see his/her own HP

--- spec/features/view_hit_points_spec.rb
+++ spec/features/view_hit_points_spec.rb
@@ -3,4 +3,9 @@ feature 'Veiwing players hit points' do
     sign_in_and_play
     expect(page).to have_content 'AK: 100 HP'
   end
+
+  scenario 'Player 1 can see his/her ownhit points' do
+    sign_in_and_play
+    expect(page).to have_content 'Sara: 100 HP'
+  end
 end
