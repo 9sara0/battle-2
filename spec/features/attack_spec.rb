63853a0bb804078b32d6d58145999e2adcc6422b
Sara <sarah.alalaya@gmail.com>
2017-04-12 20:32:57 +0300

    Refactored

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -7,26 +7,22 @@ feature 'Attacking' do
 
   scenario 'reduce player_2 HP by 10' do
     sign_in_and_play
-    click_link 'Attack'
-    click_button 'OK'
+    attack_and_confirm
     expect(page).not_to have_content 'AK: 100'
     expect(page).to have_content 'AK: 90'
   end
 
   scenario 'reduce player_1 HP by 10' do
     sign_in_and_play
-    click_link 'Attack'
-    click_button 'OK'
-    click_link 'Attack'
-    click_button 'OK'
+    attack_and_confirm
+    attack_and_confirm
     expect(page).not_to have_content 'Sara: 100 HP'
     expect(page).to have_content 'Sara: 90 HP'
   end
 
   scenario 'be attacked by player_2' do
     sign_in_and_play
-    click_link 'Attack'
-    click_button 'OK'
+    attack_and_confirm
     click_link 'Attack'
     expect(page).to have_content 'AK attacked Sara'
   end
