c4c4e472d59e299207893dee4d85836361649a38
Sara <sarah.alalaya@gmail.com>
2017-04-12 20:18:11 +0300

    Player_1 loses points when player_2 attacks

--- lib/game.rb
+++ lib/game.rb
@@ -5,7 +5,6 @@ class Game
     @player_1 = player_1
     @player_2 = player_2
     @current_turn = player_1
-
   end
 
   def attack(player)
@@ -16,7 +15,6 @@ class Game
     @current_turn = opponent_of(current_turn)
   end
 
-  private
 
   def opponent_of(the_player)
     return player_1 if the_player == player_2
--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,5 +1,5 @@
 feature 'Attacking' do
-  scenario 'attack palyer 2' do
+  scenario 'attack palyer_2' do
     sign_in_and_play
     click_link 'Attack'
     expect(page).to have_content 'Sara attacked AK'
@@ -11,6 +11,16 @@ feature 'Attacking' do
     click_button 'OK'
     expect(page).not_to have_content 'AK: 100'
     expect(page).to have_content 'AK: 90'
+  end
+
+  scenario 'reduce player_1 HP by 10' do
+    sign_in_and_play
+    click_link 'Attack'
+    click_button 'OK'
+    click_link 'Attack'
+    click_button 'OK'
+    expect(page).not_to have_content 'Sara: 100 HP'
+    expect(page).to have_content 'Sara: 90 HP'
   end
 
   scenario 'be attacked by player_2' do
