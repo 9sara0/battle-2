bf62b0c07a1cf663cbcf838767df1fd2ede3481f
Sara <sarah.alalaya@gmail.com>
2017-04-12 19:43:43 +0300

    Player_1 can be attacked by player_2 and will get confirmation

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -2,14 +2,22 @@ feature 'Attacking' do
   scenario 'attack palyer 2' do
     sign_in_and_play
     click_link 'Attack'
-    expect(page).to have_content 'You attacked AK'
+    expect(page).to have_content 'Sara attacked AK'
   end
 
   scenario 'reduce player_2 HP by 10' do
     sign_in_and_play
     click_link 'Attack'
-    click_link 'OK'
+    click_button 'OK'
     expect(page).not_to have_content 'AK: 100'
     expect(page).to have_content 'AK: 90'
+  end
+
+  scenario 'be attacked by player_2' do
+    sign_in_and_play
+    click_link 'Attack'
+    click_button 'OK'
+    click_link 'Attack'
+    expect(page).to have_content 'AK attacked Sara'
   end
 end
